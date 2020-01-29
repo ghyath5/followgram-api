78279f67f8e7d6a6a8a9ddfa6e2471782dc71345
Glitch (hello-express) <none>
2020-01-29 10:57:24 +0000

    👔🎏 Checkpoint ./package.json:609150/2634 ./server.js:609150/1493 ./views/index.html:609150/1820

--- server.js
+++ server.js
@@ -3,20 +3,21 @@
 
 // init project
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
+
+// body parser to parse JSON request payloads
+app.use(bodyParser.json());
 
 // we've started you off with Express,
 // but feel free to use whatever libs or frameworks you'd like through `package.json`.
 
-// http://expressjs.com/en/starter/static-files.html
-app.use(express.static("public"));
-
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function(request, response) {
-  response.sendFile(__dirname + "/views/index.html");
+app.get("/", (req, res) => {
+  res.send("Hello");
 });
 
 // listen for requests :)
-const listener = app.listen(process.env.PORT, function() {
+const listener = app.listen(process.env.PORT, () => {
   console.log("Your app is listening on port " + listener.address().port);
 });
