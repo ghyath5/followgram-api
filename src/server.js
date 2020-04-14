0f5a46fdf0037d1c622b691d5fb60644a4d9b2a9
Glitch (hello-express) <none>
2020-04-14 07:44:29 +0000

    📚🕵 Checkpoint ./src/server.js:609150/1233

--- src/server.js
+++ src/server.js
@@ -7,6 +7,20 @@ const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
+app.post('/addNumbers', (req, resp) => {
+  const { numbers } = req.body.input;
+  try {
+    return resp.json({
+      sum: numbers.reduce((s, n) => s + n, 0)
+    });
+  } catch(e) {
+    console.error(e)
+    return resp.status(500).json({
+      message: 'unexpected'
+    })
+  }
+});
+
 app.post('/hello', async (req, res) => {
   return res.json({
     hello: "world"
