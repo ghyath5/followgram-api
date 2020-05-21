47fc592a2e7f256193d2d0c9a2ebd18d4aaf6b88
Glitch (hello-express) <none>
2020-05-21 09:57:52 +0000

    🕹🏭 Checkpoint ./src/server.js:609150/707

--- src/server.js
+++ src/server.js
@@ -7,32 +7,11 @@ const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
-app.post('/login', (req, resp) => {
-  // You can access their arguments input at req.body.input
-  const { username, password } = req.body.input;
+// paste the code from codegen here
 
-  // perform your custom business logic
-  // check if the username and password are valid and login the user
 
-  // return the response
-  return resp.json({
-    accessToken: "Ew8jkGCNDGAo7p35RV72e0Lk3RGJoJKB"
-  })
-})
 
-app.post('/addNumbers', (req, resp) => {
-  const { numbers } = req.body.input;
-  try {
-    return resp.json({
-      sum: numbers.reduce((s, n) => s + n, 0)
-    });
-  } catch(e) {
-    console.error(e)
-    return resp.status(500).json({
-      message: 'unexpected'
-    })
-  }
-});
+
 
 app.post('/hello', async (req, res) => {
   return res.json({
