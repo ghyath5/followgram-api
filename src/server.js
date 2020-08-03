ec7df0f57b69b6612e488433a19150db0eef4d11
Glitch (hello-express) <none>
2020-08-03 16:34:52 +0000

    📻🚃 Checkpoint ./src/server.js:19975497/1175 ./package.json:19975497/2393

--- src/server.js
+++ src/server.js
@@ -4,7 +4,8 @@ const bodyParser = require("body-parser");
 const app = express();
 
 const PORT = process.env.PORT || 3000;
-
+import { IgApiClient, IgLoginTwoFactorRequiredError } from 'instagram-private-api';
+import jwt from 'jsonwebtoken'
 app.use(bodyParser.json());
 
 // paste the code from codegen here
@@ -13,10 +14,26 @@ app.use(bodyParser.json());
 
 
 
-app.post('/hello', async (req, res) => {
+// Request Handler
+app.post('/login', async (req, res) => {
+
+  // get request input
+  const { username, password } = req.body.input;
+
+ const ig = new IgApiClient();
+  ig.state.generateDevice(username);
+  let user = await ig.account.login(username, password)
+  console.log(user)
+  // return res.status(400).json({
+  //   message: "error happened"
+  // })
+
+  // success
   return res.json({
-    hello: "world"
-  });
+    token: "<value>",
+    user_id: "<value>"
+  })
+
 });
 
 app.listen(PORT);
