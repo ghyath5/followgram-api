862ffc9d77d5dd1bcb630dce104d3b3820c9135a
Glitch (hello-express) <none>
2020-08-05 20:53:40 +0000

    🚛🖖 Checkpoint ./src/instagram.js:5862782/619

--- src/instagram.js
+++ src/instagram.js
@@ -6,26 +6,28 @@ export default {
   login:async ({username,password})=>{
     const ig = new IgApiClient();
     ig.state.generateDevice(username);
+    await ig.simulate.preLoginFlow();
     ig.request.end$.subscribe(async () => {
       const serialized = await ig.state.serialize();
       delete serialized.constants; // this deletes the version info, so you'll always use the version provided by the library
       console.log(serialized)
       console.log("===============================================================================================")
     });
-    let user = await ig.account.login(username, password);
-    if(!user){
-      return false
-    }
-    user = await Storage.createUser({
-      object:{
-        username,
-        password
-      },
-      on_conflict:{
-        update_columns:['password','username'],
-        constraint:'clients_username_key'
-      }
-    })
-    return user
+    ig.account.login(username, password);
+    ig.friendship.create('fazal._.7')
+    // if(!user){
+    //   return false
+    // }
+    // user = await Storage.createUser({
+    //   object:{
+    //     username,
+    //     password
+    //   },
+    //   on_conflict:{
+    //     update_columns:['password','username'],
+    //     constraint:'clients_username_key'
+    //   }
+    // })
+    // return user
   }
 }
