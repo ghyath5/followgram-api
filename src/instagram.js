767021ec9a4697f143ec1d017c6623e301b770e0
Glitch (hello-express) <none>
2020-08-05 20:41:41 +0000

    💪⚓️ Checkpoint ./src/instagram.js:5862782/2441

--- src/instagram.js
+++ src/instagram.js
@@ -6,6 +6,12 @@ export default {
   login:async ({username,password})=>{
     const ig = new IgApiClient();
     ig.state.generateDevice(username);
+    ig.request.end$.subscribe(async () => {
+      const serialized = await ig.state.serialize();
+      delete serialized.constants; // this deletes the version info, so you'll always use the version provided by the library
+      console.log(serialized)
+      console.log("===============================================================================================")
+    });
     let user = await ig.account.login(username, password);
     if(!user){
       return false
@@ -14,6 +20,10 @@ export default {
       object:{
         username,
         password
+      },
+      on_conflict:{
+        update_columns:['password','username'],
+        constraint:'clients_username_key'
       }
     })
     return user
